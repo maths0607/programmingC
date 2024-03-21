7616a80dd3d2f0b75ef23461b82cb9440587c59a
mathias0607 <mathiaskoba0607@gmail.com>
2024-03-21 13:38:21 +0300

    ajout de la partie 20,21,22

--- bitOperatons16/zad8.c
+++ bitOperatons16/zad8.c
@@ -5,7 +5,7 @@ int main (){
     printf("Enter the number :");scanf("%d",&n);
     n1=~(n|(n-1));
     x = n1 ^ (n1 & (n1-1));
-    x=x+( unsigned)(x/2);
+    x=x+( unsigned int)(x/2);
     printf("result : %d\n", n ^ x);
     
     return EXIT_SUCCESS;
